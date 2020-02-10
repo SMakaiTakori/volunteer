738b6268ed2d52f0cf4b4e6ce2d8149e4386fb29
SMakaiTakori <makait87@icloud.com>
2020-02-10 13:50:58 -0500

    refractored self.scrape_titles method to reflect key/value pairs

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,15 +6,22 @@ class Volunteer::Scraper
 BASE_PATH = "https://www.volunteermatch.org/search/?l=Atlanta,%20GA,%20USA&categories=39"
       
 
-def self.scrape_titles
-    main_page = Nokogiri::HTML(open(BASE_PATH))
-    titles = []
-    main_page.css("div.search-page").each do |title|
-        title.css("a")[0].text.strip
-        title.css("a")[1].text 
-        binding.pry
-    end
-    titles
-end
-    
+    def self.scrape_titles
+        main_page = Nokogiri::HTML(open(BASE_PATH))
+    #     titles = []
+    #     main_page.css("div.search-page").each do |title|
+    #         title.css("a")[0].text.strip
+    #         title.css("a")[1].text 
+    #         binding.pry
+    #     end
+    #     titles
+    # end
+
+        main_page.css("div.search-page").collect do |info|
+            {:title => info.css("a")[0].text.strip,
+            :organization => info.css("a")[1].text 
+            }
+            binding.pry
+        end   
+    end 
 end
