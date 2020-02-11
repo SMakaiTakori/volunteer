d866039e480e7dad32e07cb725318d5c25210514
SMakaiTakori <makait87@icloud.com>
2020-02-10 22:30:30 -0500

    added target key/value for extra url needed to scrape further details

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,16 +12,30 @@ BASE_PATH = "https://www.volunteermatch.org/search/?l=Atlanta,%20GA,%20USA&categ
     #     main_page.css("div.search-page").each do |title|
     #         title.css("a")[0].text.strip
     #         title.css("a")[1].text 
-    #         binding.pry
+    #    
     #     end
     #     titles
     # end
 
         main_page.css("div.search-page").collect do |info|
             {:title => info.css("a")[0].text.strip,
-            :organization => info.css("a")[1].text 
+            :organization => info.css("a")[1].text,
+            :learn_more => info.css("a").attr("href").value             
             }
             binding.pry
         end   
     end 
+
+    def self.scrape_details
+        details_page = Nokogiri::HTML(open(learn_more))
+        details_page.css("logistics.logistics--box text-sub").collect do |details|
+            {
+
+
+            }
+            
+        end
+    end
+
+
 end
