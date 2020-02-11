a81b3f019d6fcc70bac1f77451d0db53da68d6df
SMakaiTakori <makait87@icloud.com>
2020-02-11 12:11:41 -0500

    found first selector for second scraping method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,22 +12,21 @@ BASE_PATH = "https://www.volunteermatch.org/search/?l=Atlanta,%20GA,%20USA&categ
             {:title => info.css("a")[0].text.strip,
             :organization => info.css("a")[1].text,
             :learn_more => info.css("a").attr("href").value             
-            }   
-          
-                  
+            }              
+                   
         end   
     end 
 
     def self.scrape_details(learn_more)
         details_page = Nokogiri::HTML(open("https://www.volunteermatch.org" + learn_more))
-        binding.pry 
-        details_page.css("div.grid.grid--container.justify-space-between.opp-dtl").collect do |details|
-            {:description => details.css("div.opp-dtl__summary p").text,
-             :address => details.css(""),
-             :date => details.css(""),
-             :requirements => details.css("") 
-            }
         
+        details_page.css("div.grid.grid--container.justify-space-between.opp-dtl").collect do |details|           
+            {:description => details.css("div#short_desc").text.strip
+            #  :address => details.css(""),
+            #  :date => details.css(""),
+            #  :requirements => details.css("") 
+            }
+            binding.pry
              
         end
     end
