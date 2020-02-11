334bf0a34bb7ab4f02619b909c243f0e80b1c9a9
SMakaiTakori <makait87@icloud.com>
2020-02-11 11:02:23 -0500

    added initialize method in volunteer class

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -2,15 +2,27 @@ class Volunteer::Volunteer
 
 attr_accessor :title, :organization, :address, :description, :date, :requirements, :learn_more
 
-@@all = []
+    @@all = []
 
-def initialize
-end
-
-def self.create_from_collection(list_array)
-    list_array.each do |hash|
-        Volunteer::Volunteer.new(hash)
+    def initialize(attributes)
+        attributes.each {|key, value}
+        begin
+            self.send(("#{key}="), value)
+            @@all << self
+        rescue NoMethodError
+            
+        end
+        }        
     end
-end
+
+    def self.create_from_collection(list_array)
+        list_array.each do |hash|
+            Volunteer::Volunteer.new(hash)
+        end
+    end
+
+    def self.all
+        @all
+    end
     
 end
