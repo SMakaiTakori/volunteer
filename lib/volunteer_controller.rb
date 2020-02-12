51e4db4dbcd1ec87ad4435baac471701ce2958cb
SMakaiTakori <makait87@icloud.com>
2020-02-12 13:01:44 -0500

    deleted argument and refractored get_list method, refractored display_list method

--- lib/volunteer_controller.rb
+++ lib/volunteer_controller.rb
@@ -10,7 +10,7 @@ class Volunteer::VolunteerController
         input = gets.strip
         #will take in input and return a list of volunteer opportunities/titles and organization names
        make_list
-       get_list(input)
+       get_list
     end
     
     def prompt
@@ -35,23 +35,24 @@ class Volunteer::VolunteerController
     end
 
 
-    def get_list(input)
+    def get_list
         
-        puts "Here is a list of opportunities and organizations looking for volunteers in Atlanta, GA : "
+        
         input = gets.strip
         
         if input == 'list'
-            self.display_list
+            puts "Here is a list of opportunities and organizations looking for volunteers in Atlanta, GA : "
+            display_list
         else 
             puts "Invalid input, please try again."
-            self.get_list
+            get_list
         end      
     end
 
     def display_list
-        self.add_list.each_with_index do |title, index|
-            puts "#{index + 1}. #{title[title][organization]}"
-            binding.pry
+        Volunteer::Volunteer.all.each.with_index do |title, index|
+            puts "#{index + 1}. #{title.title}"
+        # binding.pry
         end
     end
 
